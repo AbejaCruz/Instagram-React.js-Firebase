09141b77a82bbed6a3d1f26fc25c6f7374b1061f
AbejaCruz <abeja.dact_@hotmail.com>
2017-03-12 13:03:32 -0500

    subo funcionalidad de subir imagenes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,76 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import FileUpload from './FileUpload';
 import './App.css';
+
 
 class App extends Component {
 
-  constructor (){
+  constructor(){
     super();
-    this.state = {
+    this.state={
       user: null
     };
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
   }
 
-  ComponentWillMount(){
-    firebase.auth().onAuthStateChanged(user =>{
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user=>{
       this.setState({ user });
-    })
+    });
   }
-  handleAuth() { 
-    const provider = new firebase.auth.GoogleAuthProvider();
 
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
     .then(result => console.log(`${result.user.email} ha iniciado sesión`))
     .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
-  handleLogout (){
+  handleLogout(){
     firebase.auth().signOut()
     .then(result => console.log(`${result.user.email} ha salido`))
     .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
-  renderLoginButton(){
-    
-    //si elusuario está logueado
-    if (this.state.user) {
-      return(
+  renderLoginButton (){
+    // Si el usuario está logeado
+    if (this.state.user){
+      return (
         <div>
-        <img width="100" src={this.state.user.photoURL} alt={this.user.displayName}/>
-        <p>Hola {this.state.user.displayName}!</p>
-        <button onClick={this.handleLogout}> Salir </button>
-        </div>
-        );
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Hola {this.state.user.displayName} ! </p>
+          <button onClick={this.handleLogout}>Salir</button>
+          <FileUpload/>
 
-    } else {
-    //Si no lo está
-    return(
-      <button onClick={this.handleAuth}> Login Con Google </button>
-     );
+        </div>
+
+
+
+      );
+    }else{
+      return(
+        <button onClick={this.handleAuth}>Login con Google</button>
+      );
     }
-  
+
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Instagram</h2>
+          <h2>Pseudogram</h2>
         </div>
         <p className="App-intro">
-
-        {this.renderLoginButton() }
+          {this.renderLoginButton()}
         </p>
       </div>
     );
   }
-}
+
+
+}//Class
 
 export default App;
